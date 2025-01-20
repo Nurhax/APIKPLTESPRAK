38d437ee5a3610b9ecb81fb5477b9634b085d443
Nurhax <iqbalnur2009@gmail.com>
2025-01-20 14:10:36 +0700

    commit pertama, menambahkan API menggunakan List dan JSON

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TesprakKPL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private static List<Student> _students = new List<Student>
+        {
+            new Student("Iqbal Nurhaq", "PS5", 1, 8000000, "2025-01-20", "JL Pegangsaan Timur 2", "Pending"),
+            new Student("Iqbal Nurhaq", "NS1", 2, 5000000, "2025-01-20", "JL Pegangsaan Timur 22", "Shipped"),
+            new Student("Iqbal Nurhaq", "NS2", 3, 9000000, "2025-01-20", "JL Pegangsaan Timur 23", "Processed"),
+            new Student("Iqbal Nurhaq", "PS4", 4, 3000000, "2025-01-20", "JL Pegangsaan Timur 24", "Delivered"),
+            new Student("Iqbal Nurhaq", "PS3", 5, 2000000, "2025-01-20", "JL Pegangsaan Timur 25", "Pending")
+        };
+
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+
+        [HttpGet("{id}")]
+        public Student GetStudent(int id)
+        {
+            return _students[id];
+        }
+
+        [HttpPost]
+        public void PostStudent([FromBody] Student student)
+        {
+            _students.Add(student);
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateStudent(int id, [FromBody] Student student)
+        {
+            _students[id] = student;
+        }
+
+        [HttpDelete]
+        public void DeleteStudent(int id)
+        {
+            _students.RemoveAt(id);
+        }
+
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿namespace TesprakKPL
+{
+    public class Student
+    {
+        public string? Customer_name { get; set; }
+        public string? Product_name { get; set; }
+        public int Quantity { get; set; }
+        public int Total_price { get; set; }
+        public string? Order_date { get; set; }
+        public string? Shipping_address { get; set; }
+        public string? Status { get; set; }
+
+        public Student() { }
+
+        public Student(string? customer_name, string? product_name, int quantity, int total_price, string order_date, string? shipping_address, string? status)
+        {
+            Customer_name = customer_name;
+            Product_name = product_name;
+            Quantity = quantity;
+            Total_price = total_price;
+            Order_date = order_date;
+            Shipping_address = shipping_address;
+            Status = status;
+        }
+    }
+}
